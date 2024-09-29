917fd8e99763c82de26a109d62e8aa5e3f9e583e
JEETHESH <jeetheshc@gmail.com>
2024-09-29 20:40:53 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+const express = require('express');
+const { connectDB } = require('./config/db.js');
+const app = express();
+const port = 3000;
+connectDB();
 app.get('/', (req, res) => {
-  res.send('Hello World1!')
-})
+  res.send('hello world');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
